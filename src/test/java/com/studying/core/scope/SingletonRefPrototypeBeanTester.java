89f6501158c11fc37eb4038ddb92db66b3c5f90a
junwei.zhang <mestuding@sina.com>
2015-03-21 17:44:43 +0800

    初始化项目附带springmvc小例子

---
+++ src/test/java/com/studying/core/scope/SingletonRefPrototypeBeanTester.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2010-2015 meituan.com
+ * All rights reserved.
+ * 酒店后台研发.
+ */
+package com.studying.core.scope;
+/**
+ * 单例bean引用原型bean.如:在迁移groupapi时,将restlet方式改为springmvc,原有的resource都是带状态的,需要为单例.<br />
+ * 参考:<a href="http://stackoverflow.com/questions/3891997/how-to-do-spring-lookup-method-injection-with-annotations">
+ * how-to-do-spring-lookup-method-injection-with-annotations</a><br />
+ * @author: zhangjunwei@meituan.com
+ * @Date: 2015年3月21日
+ */
+public class SingletonRefPrototypeBeanTester {
+	
+}
