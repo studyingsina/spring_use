4b667414a6bab7364c9ac6468ad2d5534ef43e4e
zhangjunwei <mestudying@sina.com>
2017-09-12 14:08:16 +0800

    add comment

--- src/test/java/com/studying/aop/AopTester.java
+++ src/test/java/com/studying/aop/AopTester.java
@@ -7,6 +7,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Recover the bug : https://jira.spring.io/browse/SPR-14241
+ *
+ * For recover this bug: add a breakpoint in @see AbstractAspectJAdvice 621 line, for example:
+ * <code>
+ *     return this.aspectJAdviceMethod.invoke(this.aspectInstanceFactory.getAspectInstance(), actualArgs);
+ * </code>
+ * and let thread-2 go into getAspectInstance() and gaps the lock, but don't create bean; then switch to thread-1 in the IDE debug view,
+ * and let thread-1 which has gapped the spring singleton map lock execute afterPropertiesSet() method and
+ * initialize the same aspect @see Operator, thread-1 will wait the lock when it get into getAspectInstance().
+ *
  */
 public class AopTester {
 
